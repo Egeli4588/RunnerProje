0f4062caac575adbc3cc69f86a8645c81a6d7f6f
Egeli4588 <ali_uysun45@hotmail.com>
2025-08-11 12:19:45 +0300

    ciktiAlinmisHali

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,95 +7,66 @@ public enum Positions
     onLeft,
     onMiddle,
     onRight
-
 }
+
 public class PlayerController : MonoBehaviour
 {
     [Header("Elements")]
     [SerializeField] Rigidbody rb;
     [SerializeField] public Animator myAnim;
-    //sesler için
     [SerializeField] AudioClip bonusSound, CoinSound, DeathSound, MagnetCoinSound, ShieldSound, WinSound;
-    [SerializeField] AudioSource PlayerSound;//oyuncu Sesleri için
-    //VfX ler için
-    [SerializeField] GameObject coinCollectedVFX, deathVFX, healthDeclineVFX, magnetVFX, wallBreakVFX,ShieldVFX;
+    [SerializeField] AudioSource PlayerSound;
+    [SerializeField] GameObject coinCollectedVFX, deathVFX, healthDeclineVFX, magnetVFX, wallBreakVFX, ShieldVFX;
 
     [Header("Settings")]
-    [Tooltip("bu deðiþken oyuncunun hýzýný belirler")]
     [SerializeField] float speed;
     [SerializeField] float shift = 2;
-
     [SerializeField] bool isLeft, isMiddle, isRight;
 
     public bool isDead;
-
     public bool isStart;
 
     [SerializeField] public int score;
-
     [SerializeField] public float floatScore;
     [SerializeField] public float passedTime;
 
-    // Enum tanýmlam
-
     [System.NonSerialized] public Positions positions = Positions.onMiddle;
 
-
-    //þimdi boollarý tanýmlayalým.
-
     public bool is2XActive, isShieldActive, isMagnetActive;
-    //saðlýk ekleyelim.
     [SerializeField] int Health;
 
-
     float beforeSpeed;
-
-
     bool isMove;
-    //zýplama için deneme1
+
+    // Zýplama
     bool isJumping = false;
     float jumpTimer = 0f;
     [SerializeField] float jumpHeight = 2f;
     [SerializeField] float jumpDuration = 0.5f;
-    float initialY; // zýplama öncesi y konumu
+    float initialY;
 
+    // Dokunma kontrolü için
+    Vector2 touchStartPos;
+    bool swipeDetected = false;
 
     void Start()
     {
-        // transform.position = new Vector3(0, 0, 1);
         isMiddle = true;
-        // myAnim.SetBool("Run", true);
-
         initialY = transform.position.y;
     }
 
-    // Update is called once per frame
     void Update()
     {
         passedTime += Time.deltaTime;
         moveCharacter();
-
     }
 
-    /* private void FixedUpdate()
-     {
-         rb.MovePosition(transform.position + Vector3.forward * speed * Time.deltaTime);
-     }*/
-
-    /// <summary>
-    /// bu metod karakter hareketi ve sýnýrlandýrma iþlemlerini yapar.
-    /// </summary>
     void moveCharacter()
     {
-
-        if (!isStart) return;
-        if (isDead) return;
-
+        if (!isStart || isDead) return;
 
         if (is2XActive)
-        {
             floatScore += Time.deltaTime;
-        }
 
         floatScore += Time.deltaTime;
         if (floatScore > 1)
@@ -103,141 +74,35 @@ public class PlayerController : MonoBehaviour
             score += 1;
             floatScore = 0;
         }
+
         if (passedTime > 10)
         {
             speed += 0.3f;
             passedTime = 0;
         }
 
-        #region karakter sýnýrlama yöntemleri
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
-
-        if (Input.GetKeyDown(KeyCode.A) && transform.position.x > -0.5f && !isMove)
+        // Giriþ kontrolü: PC ve Mobil
+        if ((Input.GetKeyDown(KeyCode.A) || IsLeftSideTouched()) && transform.position.x > -0.5f && !isMove)
         {
-            // transform.Translate(new Vector3(-shift, 0, 0));
             transform.DOMoveX(transform.position.x - shift, 0.5f).SetEase(Ease.Linear).OnComplete(isMoveToFalse);
-
             isMove = true;
         }
-        else if (Input.GetKeyDown(KeyCode.D) && transform.position.x < 0.5f && !isMove)
+        else if ((Input.GetKeyDown(KeyCode.D) || IsRightSideTouched()) && transform.position.x < 0.5f && !isMove)
         {
-            // transform.Translate(new Vector3(shift, 0, 0));
             transform.DOMoveX(transform.position.x + shift, 0.5f).SetEase(Ease.Linear).OnComplete(isMoveToFalse);
             isMove = true;
         }
 
-
-        /* if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && positions != Positions.onLeft)
-
-         {
-             if (positions == Positions.onMiddle)
-             {
-                 positions = Positions.onLeft;
-
-             }
-             else if (positions == Positions.onRight)
-             {
-                 positions = Positions.onMiddle;
-
-             }
-             transform.Translate(new Vector3(-shift, 0, 0));
-
-
-         }
-         else if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && positions != Positions.onRight)
-         {
-             if (positions == Positions.onMiddle)
-             {
-                 positions = Positions.onRight;
-             }
-             else if (positions == Positions.onLeft)
-             {
-                 positions = Positions.onMiddle;
-
-             }
-             transform.Translate(new Vector3(shift, 0, 0));
-         }
-
-         */
-
-
-
-
-        /*  if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && isLeft == false)
-
-          {
-              if (isMiddle)
-              {
-                  isLeft = true;
-                  isMiddle = false;
-              }
-              else if (isRight)
-              {
-                  isMiddle = true;
-                  isRight = false;
-
-              }
-              transform.Translate(new Vector3(-shift, 0, 0));
-
-
-          }
-          else if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && isRight == false)
-          {
-              if (isMiddle)
-              {
-                  isRight = true;
-                  isMiddle = false;
-              }
-              else if (isLeft)
-              {
-                  isMiddle = true;
-                  isLeft = false;
-
-              }
-              transform.Translate(new Vector3(shift, 0, 0));
-          }
-
-          */
-
-
-        /*  if (Input.GetKey(KeyCode.A))
-          {
-              myAnim.SetBool("Run", true);
-          }
-          else if (Input.GetKeyUp(KeyCode.A))
-          {
-              myAnim.SetBool("Run", false);
-          }*/
-        /* if (Input.GetKeyDown(KeyCode.Space)) 
-         {
-             myAnim.SetBool("Jump", true);
-         }
-         else if (Input.GetKeyUp(KeyCode.Space))
-         {
-             myAnim.SetBool("Jump", false);
-         }
-       */
-        //hareket 1.yol.
-
-
-
-
-
-        // hareket 2.yol.
-        #endregion
-
-        // yeni zýplama için 
-        // Zýplama baþlat
-        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        // Zýplama tetikleme
+        if ((Input.GetKeyDown(KeyCode.Space) || IsSwipeUp()) && !isJumping)
         {
             isJumping = true;
             jumpTimer = 0f;
-            // myAnim.SetBool("Jump", true);
             myAnim.SetTrigger("JumpTrigger");
         }
 
-        // Zýplama iþlemi (yukarý-aþaðý hareket)
         if (isJumping)
         {
             jumpTimer += Time.deltaTime;
@@ -250,29 +115,71 @@ public class PlayerController : MonoBehaviour
 
             if (jumpTimer >= jumpDuration)
             {
-                // Zýplama tamamlandý
                 isJumping = false;
                 transform.position = new Vector3(transform.position.x, initialY, transform.position.z);
                 myAnim.SetBool("Jump", false);
             }
         }
-
-
-
-
     }
-
 
     void isMoveToFalse()
     {
-
         isMove = false;
-
     }
+
+    #region Dokunma Giriþleri
+    bool IsLeftSideTouched()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began && touch.position.x < Screen.width / 2)
+                return true;
+        }
+        return false;
+    }
+
+    bool IsRightSideTouched()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began && touch.position.x >= Screen.width / 2)
+                return true;
+        }
+        return false;
+    }
+
+    bool IsSwipeUp()
+    {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                touchStartPos = touch.position;
+                swipeDetected = true;
+            }
+            else if (touch.phase == TouchPhase.Ended && swipeDetected)
+            {
+                Vector2 swipeDelta = touch.position - touchStartPos;
+                swipeDetected = false;
+
+                if (swipeDelta.y > 100 && Mathf.Abs(swipeDelta.x) < 100) // Yukarý kaydýrma
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    #endregion
+
+    #region Çarpýþma ve Toplama Kodlarý (Deðiþmedi)
+
     private void OnCollisionEnter(Collision other)
     {
-        Debug.Log("çarpýþtýk");
-
         if (other.gameObject.CompareTag("obstacle"))
         {
             int damage = other.gameObject.GetComponent<Obstacle>().damage;
@@ -283,13 +190,11 @@ public class PlayerController : MonoBehaviour
                 isShieldActive = false;
                 GameObject vfx = Instantiate(wallBreakVFX, other.transform.position, Quaternion.identity);
                 Destroy(vfx, 1f);
-
             }
             else
             {
                 CheckHealth(damage, other.gameObject);
             }
-
         }
     }
 
@@ -298,32 +203,19 @@ public class PlayerController : MonoBehaviour
         Health -= damage;
         if (Health <= 0)
         {
-            Debug.Log("çarpýþtýk");
             myAnim.SetBool("Death", true);
             PlayerSound.PlayOneShot(DeathSound);
-            // GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.Euler(-90,0,0));
             GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
             Destroy(vfx, 1f);
-
             isDead = true;
         }
         else
         {
-            Destroy(other.gameObject);
-            GameObject vfx = Instantiate(wallBreakVFX, other.transform.position, Quaternion.identity);
-            Destroy(vfx, 1f);
-
-            GameObject healthvfx = Instantiate(healthDeclineVFX, transform.position, Quaternion.identity,this.transform);
+            Destroy(other);
+            Instantiate(wallBreakVFX, other.transform.position, Quaternion.identity);
+            GameObject healthvfx = Instantiate(healthDeclineVFX, transform.position, Quaternion.identity, this.transform);
             Destroy(healthvfx, 1f);
-
-
-
         }
-      /*  if (score>300)
-        {
-            PlayerSound.PlayOneShot(WinSound);
-        }
-      */
     }
 
     private void OnTriggerEnter(Collider other)
@@ -336,7 +228,6 @@ public class PlayerController : MonoBehaviour
                 case CollectablesEnum.Coin:
                     AddScrore(collectables.toBeAddedScore);
                     break;
-
                 case CollectablesEnum.Shield:
                     ActivateShield();
                     break;
@@ -349,15 +240,12 @@ public class PlayerController : MonoBehaviour
                 case CollectablesEnum.SpeedUp:
                     AddSpeed(collectables.toBeAddedSpeed);
                     break;
-
                 case CollectablesEnum.Magnet:
                     ActivateMagnet();
-
                     break;
-
             }
 
-            Destroy(other.gameObject);// coinlerin yok olmasý
+            Destroy(other.gameObject);
         }
     }
 
@@ -368,39 +256,32 @@ public class PlayerController : MonoBehaviour
         Invoke("BackToOrijinalSpeed", 5f);
     }
 
-
     void BackToOrijinalSpeed()
     {
-
         speed = beforeSpeed;
     }
+
     void AddScrore(int TobeAddedScore)
     {
         if (isMagnetActive)
         {
-            PlayerSound.clip = MagnetCoinSound;//magnetse
-           
-                PlayerSound.Play();
-            
+            PlayerSound.clip = MagnetCoinSound;
+            PlayerSound.Play();
         }
         else
         {
-            PlayerSound.clip = CoinSound;//coinse
-            
-                PlayerSound.Play();
-            
+            PlayerSound.clip = CoinSound;
+            PlayerSound.Play();
         }
 
-
-        // GameObject vfx= Instantiate(coinCollectedVFX, transform.position, Quaternion.identity);
-        //  GameObject vfx= Instantiate(coinCollectedVFX, transform.position, Quaternion.identity,this.transform);
-        GameObject vfx = Instantiate(coinCollectedVFX, transform.position+ new Vector3(0,1,0), Quaternion.identity, this.transform);
+        GameObject vfx = Instantiate(coinCollectedVFX, transform.position + new Vector3(0, 1, 0), Quaternion.identity, this.transform);
         Destroy(vfx, 1f);
 
         if (is2XActive)
         {
             TobeAddedScore *= 2;
         }
+
         score += TobeAddedScore;
     }
 
@@ -408,7 +289,7 @@ public class PlayerController : MonoBehaviour
     {
         isShieldActive = true;
         PlayerSound.PlayOneShot(ShieldSound);
-        GameObject vfx = Instantiate(ShieldVFX, transform.position , Quaternion.identity, this.transform);
+        GameObject vfx = Instantiate(ShieldVFX, transform.position, Quaternion.identity, this.transform);
         Destroy(vfx, 5f);
         Invoke("DeactivateShield", 5f);
     }
@@ -423,20 +304,16 @@ public class PlayerController : MonoBehaviour
         Health += ToBeAddedHealth;
         if (Health <= 0)
         {
-            Debug.Log("çarpýþtýk");
             myAnim.SetBool("Death", true);
             isDead = true;
         }
     }
-
 
     void ActivateBonus()
     {
         is2XActive = true;
         AudioSource.PlayClipAtPoint(bonusSound, transform.position);
         Invoke("DeActivateBonus", 5f);
-     
-     
     }
 
     void DeActivateBonus()
@@ -447,9 +324,8 @@ public class PlayerController : MonoBehaviour
     void ActivateMagnet()
     {
         isMagnetActive = true;
-        GameObject vfx = Instantiate(magnetVFX, this.transform.position+ new Vector3(0,1,0), Quaternion.identity, this.transform);
+        GameObject vfx = Instantiate(magnetVFX, transform.position + new Vector3(0, 1, 0), Quaternion.identity, this.transform);
         Destroy(vfx, 5f);
-
         Invoke("DeActivateMagnet", 5f);
     }
 
@@ -457,4 +333,6 @@ public class PlayerController : MonoBehaviour
     {
         isMagnetActive = false;
     }
+
+    #endregion
 }
